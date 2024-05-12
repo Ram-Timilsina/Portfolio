72a14fa829736ff25c51132bb3ab4f75184dc53b
Ram <ramtimilsina16@gmail.com>
2024-05-12 19:38:39 +0545

    portfolio

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
-const header = document.querySelector("header");
-const menu = document.querySelector('#menu-icon');
-const navlist = document.querySelector('.navlist');
+const hamburger = document.querySelector('.hamburger');
+const ul= document.querySelector('ul');
 
-window.addEventListener("scroll", () => {
-    header.classList.toggle("sticky", window.scrollY > 120);
-});
+hamburger.addEventListener('click',()=>{
+    ul.classList.toggle('ham_active');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('active');
-};
-
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navlist.classList.remove('active');
-};
-const contactButton = document.getElementById("contactButton");
-const contactInfo = document.getElementById("contactInfo");
-
-contactButton.addEventListener("click", function() {
-    // Toggle the visibility of the contact information
-    if (contactInfo.style.display === "none") {
-        contactInfo.style.display = "block";
-    } else {
-        contactInfo.style.display = "none";
+    let ham_child=hamburger.firstElementChild;
+    if(ul.classList.contains('ham_active')){
+        ham_child.classList.replace('fa-bars','fa-xmark');
+    }
+    else{
+        ham_child.classList.replace('fa-xmark','fa-bars');
     }
 });
